c0930def2c3334ef551b3b204daacf279d7eb8a7
tranhuq <tranhuq@miamioh.edu>
2022-08-24 09:05:22 -0400

    initial commit of my cse 386 work

---
+++ cse386-fall2022-base/CSE386/exercisebasicgraphics.cpp
@@ -0,0 +1,45 @@
+/****************************************************
+ * 2016-2022 Eric Bachmann and Mike Zmuda
+ * All Rights Reserved.
+ * PLEASE NOTE:
+ * Dissemination of this information or reproduction
+ * of this material is prohibited unless prior written
+ * permission is granted.
+ ****************************************************/
+
+#include <ctime>
+#include <vector>
+#include "defs.h"
+#include "utilities.h"
+#include "framebuffer.h"
+#include "colorandmaterials.h"
+#include "rasterization.h"
+#include "io.h"
+
+FrameBuffer frameBuffer(WINDOW_WIDTH, WINDOW_HEIGHT);
+
+void render() {
+	frameBuffer.clearColorBuffer();
+	drawLine(frameBuffer, 0, 0, 100, 100, red);
+	drawLine(frameBuffer, 100, 100, 200, 100, blue);
+	frameBuffer.showColorBuffer();
+}
+
+void resize(int width, int height) {
+	frameBuffer.setFrameBufferSize(width, height);
+	glutPostRedisplay();
+}
+int main(int argc, char* argv[]) {
+	graphicsInit(argc, argv, __FILE__);
+
+	glutDisplayFunc(render);
+	glutReshapeFunc(resize);
+	glutKeyboardFunc(keyboardUtility);
+	glutMouseFunc(mouseUtility);
+
+	frameBuffer.setClearColor(yellow);
+
+	glutMainLoop();
+
+	return 0;
+}
